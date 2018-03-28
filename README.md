9b3e09965189aacda9310a925ec597ac67bbfc43
Forest Anderson <wizcardforest@gmail.com>
2018-03-28 14:52:22 -0400

    Added random feature to use for tests

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# rust-modular-exponentiation-calculator
+# Rust modular exponentiation calculator
+
+## Description
+
+This is a tool to brute force the exponent needed to satisfy the equation base ^ x % modulus = remainder. It is useful for CTFs, and factoring different types of encryption. This implementation currently takes around 3 seconds to evaluate a million values.
+
+## Use
+To run, make sure you have Rust nightly installed. Then, run `cargo run --release` from the parent directory. When prompted, type "Y" for a demo using random numbers, or anything else to use your own numbers.
+
+## mod_exp.py
+This is a Python implementation for speed comparisons.
--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,61 @@ extern crate num;
 extern crate mod_pow;
 extern crate modexp;
 extern crate ramp;
+extern crate rand;
 
-use num::bigint::{ BigInt, BigUint, Sign };
-use num::traits::FromPrimitive;
+use rand::{Rng};
 use std::str::FromStr;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::io::{stdin,stdout,Write};
 
-use mod_pow::ModPow;
-use modexp::modexp;
 use ramp::Int;
 
 fn main() {
     let print_num = 1000000;
+    let ramp_base;
+    let mut i: i64;
+    let ramp_mod;
+    let ramp_goal;
+
+    println!("This will calculate x for (base ^ x mod modulus = remainder goal)");
+    if get_user_input("Use random numbers? (Y/n) ") == "Y" {
+        let mut rng = rand::thread_rng();
+        i = 0;
+        let rnd_min = 100000000;
+        let rnd_max = 999999999;
+        ramp_base = Int::from(rng.gen_range(rnd_min, rnd_max));
+        let ramp_exponent = Int::from(rng.gen_range(rnd_min, rnd_max));
+        ramp_mod = Int::from(rng.gen_range(rnd_min, rnd_max));
+        ramp_goal = ramp_base.pow_mod(&ramp_exponent, &ramp_mod);
+
+        println!("Base is {}", ramp_base);
+        println!("Exponent is {}", ramp_exponent);
+        println!("Mod is {}", ramp_mod);
+        println!("Goal is {}", ramp_goal);
+    }
+    else {
+        ramp_base = Int::from_str(&get_user_input("What is the base? ")).unwrap();
+        i = get_user_input("What should the exponent count start at? (Normally 0) ").parse().unwrap();
+        ramp_mod = Int::from_str(&get_user_input("What is the modulus? ")).unwrap();
+        ramp_goal = Int::from_str(&get_user_input("What is the remainder goal? ")).unwrap();
+    }
+
+    let mut ramp_mod_exp = Int::from_str("0").unwrap();
     let mut last_time = get_time();
 
-    let mut i = 1;
-    let goal = BigInt::new(Sign::Plus, vec![10,483,580,695,461,280,548,150,531]);
-    let mod_numU = BigUint::parse_bytes(b"15045919506432000000000001", 10).unwrap();
-    let mod_num = BigInt::new(Sign::Plus, vec![15,045,919,506,432,000,000,000,001]);
-    let mut num = BigInt::new(Sign::Plus, vec![0]);
-    let base = BigInt::new(Sign::Plus, vec![7]);
-
-    let ramp_goal = Int::from_str("10483580695461280548150531").unwrap();
-    let ramp_mod = Int::from_str("15045919506432000000000001").unwrap();
-    let ramp_base = Int::from_str("7").unwrap();
-    let ramp_num = Int::from_str("0").unwrap();
-    let mut ramp_mod_exp = Int::from_str("0").unwrap();;
-
-    while ramp_num != ramp_goal {
-        //let exponent = BigInt::from_i64(i).unwrap();
-        let ramp_exponent = Int::from(i);
-
-        //let use_mod_pow = base.mod_pow(&exponent, &mod_numU);
-        //let use_mod_exp = modexp(base, exponent, mod_num);
-        ramp_mod_exp = ramp_base.pow_mod(&ramp_exponent, &ramp_mod);
-        
+    while ramp_mod_exp != ramp_goal {
         i += 1;
+        let ramp_exponent_count = Int::from(i);
+
+        ramp_mod_exp = ramp_base.pow_mod(&ramp_exponent_count, &ramp_mod);
+        
         if i % print_num == 0 {
             let elapsed_time = get_time() - last_time;
             last_time = get_time();
-            println!("{}ms - {} {}", elapsed_time, i, ramp_mod_exp);
+            println!("{}ms - Exponent is at {}", elapsed_time, i);
         }
     }
-    println!("WINNER {} at {}", ramp_mod_exp, i);
+    println!("Found x - {} ^ {} mod {} = {}", ramp_base, i, ramp_mod, ramp_mod_exp);
 }
 
 fn get_time() -> u64 {
@@ -52,4 +64,18 @@ fn get_time() -> u64 {
     let since_the_epoch = start.duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
     return since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+}
+
+fn get_user_input(question: &str) -> String {
+    let mut s = String::new();
+    print!("{}", question);
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    return s;
 }
